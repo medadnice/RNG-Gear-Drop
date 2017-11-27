02eae9211bca8b72303ce7f85dc7e4054065ff37
medadnice <derekmedeiros@liquidlayouts.com>
2017-11-27 10:21:17 -0500

    Removed redundant code

--- RNG-Gear-Drop.py
+++ RNG-Gear-Drop.py
@@ -1,11 +1,14 @@
 import random
 
+# Set your random roll range
 random_number = random.randint(1, 50)
 print("Your random number is " + str(random_number))
 
-
+# Create dictionary for items and values
 gear = {'Arcanum of Wizardry': 10, 'Rib Cage of King Ragmussen': 20,
         'Helm of Helia': 30, 'Flametooth Dagger': 40, 'Wand of Zeek': 50}
+
+# Check random_number against items in dictionary.
 
 
 def rng():
