162621611915abde67c1b5ba9a417d8ed8aeed72
medadnice <derekmedeiros@liquidlayouts.com>
2017-11-27 10:18:34 -0500

    Removed redundant code

--- RNG-Gear-Drop.py
+++ RNG-Gear-Drop.py
@@ -3,10 +3,6 @@ import random
 random_number = random.randint(1, 50)
 print("Your random number is " + str(random_number))
 
-Arcanum_of_wizardry = 10
-chest_gear = 20
-head_piece = 30
-sword = 40
 
 gear = {'Arcanum of Wizardry': 10, 'Rib Cage of King Ragmussen': 20,
         'Helm of Helia': 30, 'Flametooth Dagger': 40, 'Wand of Zeek': 50}
